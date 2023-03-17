f3136f806f75eb0f1682011b44a08c9a2eda13be
Caroline Vieira <carolinesanvieira@gmail.com>
2023-03-18 00:35:34 +0100

    Integration with Stripe - Using SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import Image from "next/image";
 import { HomeContainer, Product } from "../styles/pages/home";
 import { useKeenSlider } from 'keen-slider/react';
-import shirt1 from '../assets/shirts/1.png';
-import shirt2 from '../assets/shirts/2.png';
-import shirt3 from '../assets/shirts/3.png';
 import 'keen-slider/keen-slider.min.css';
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import { stripe } from "../lib/stripe";
 import Stripe from "stripe";
 
@@ -30,7 +27,7 @@ export default function Home({ products } : HomeProps) {
       {products && products.map(product => {
           return (
             <Product key={product.id} className="keen-slider__slide">
-              <Image src={shirt2} width={520} height={480} alt="shirt logo 1" />
+              <Image src={product.imageUrl} width={520} height={480} alt="shirt logo 1" />
 
               <footer>
                 <strong>{product.name}</strong>
@@ -45,7 +42,7 @@ export default function Home({ products } : HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ["data.default_price"],
   });
@@ -63,6 +60,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products,
-    }
+    },
+
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
