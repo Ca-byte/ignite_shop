ddea17fe4033730002eebe2889b291f4e491a90f
Caroline Vieira <carolinesanvieira@gmail.com>
2023-03-20 23:31:08 +0100

    feat: Product & Checkout - Product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,22 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/product"
 import { useRouter } from "next/router"
 
 
 export default function Product(){
 	const { query } = useRouter()
 	return(
-		<h1>Product : {JSON.stringify(query)}</h1>
+		<ProductContainer>
+			<ImageContainer>
+
+			</ImageContainer>
+			<ProductDetails>
+				<h1>Shirt Y</h1>
+				<span>$ 29,90</span>
+
+				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi adipisci nesciunt vero aut esse labore eius quae, minima, nostrum, quasi illum iste maxime delectus minus qui ad ut libero. Quod.</p>
+
+				<button>Buy Now</button>
+			</ProductDetails>
+		</ProductContainer>
 	)
 }
