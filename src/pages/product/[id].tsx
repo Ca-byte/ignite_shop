80b4bed72a5f3469fd8e01c84c4235c3f4248241
Caroline Vieira <carolinesanvieira@gmail.com>
2023-03-21 23:51:59 +0100

    feat: Product & Checkout - Loading product data

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,22 +1,60 @@
+import { stripe } from "@/src/lib/stripe"
 import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/product"
-import { useRouter } from "next/router"
+import { GetStaticProps } from "next"
+import Image from "next/image"
+import Stripe from "stripe"
 
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+		description: string;
+  }
+}
 
-export default function Product(){
-	const { query } = useRouter()
+export default function Product({ product }: ProductProps){
+	
 	return(
 		<ProductContainer>
 			<ImageContainer>
-
+				<Image src={product.imageUrl} width={520}  height={480} alt=" black shirt"/>
 			</ImageContainer>
 			<ProductDetails>
-				<h1>Shirt Y</h1>
-				<span>$ 29,90</span>
+				<h1>{product.name}</h1>
+				<span>{product.price}</span>
 
-				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi adipisci nesciunt vero aut esse labore eius quae, minima, nostrum, quasi illum iste maxime delectus minus qui ad ut libero. Quod.</p>
+				<p>{product.description}</p>
 
 				<button>Buy Now</button>
 			</ProductDetails>
 		</ProductContainer>
 	)
+}
+
+export const getStaticProps: GetStaticProps<any, {id: string}> = async ({ params }) => {
+	const productId = params.id;
+
+	const product = await stripe.products.retrieve(productId, {
+		expand: ['defaul_price'],
+
+	})
+	const price = product.default_price as Stripe.Price
+	return {
+		props: {
+			product: {
+				id: product.id,
+				name: product.name,
+				imageUrl: product.images[0],
+				price: new Intl.NumberFormat('en-US', {
+					style: 'currency',
+					currency: 'USD'
+				}).format((price.unit_amount || 0)  / 100),
+				description: product.description, 
+			}
+
+		},
+		revalidate: 60 * 60 * 1 // 1 hour
+	}
 }
