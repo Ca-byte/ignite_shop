95930b8525ff052434d461b9e316ae1d8ad09c9a
Caroline Vieira <carolinesanvieira@gmail.com>
2023-03-29 21:29:19 +0200

    feat: Product & Checkout - Success screen

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success(){
 	return(
-		<h1>Success</h1>
+		<SuccessContainer>
+			<h1>Thank you!</h1>
+
+			<p>Payment done successfully!</p>
+
+			<ImageContainer>
+
+			</ImageContainer>
+		<p>
+			yupiii <strong>Caroline Vieira</strong>, your <strong>T-shirt Beyond the limits</strong> is already on your way home.
+		</p>
+
+		<Link href='/'>
+			Back Shopping
+		</Link>
+
+		</SuccessContainer>
 	)
 }
